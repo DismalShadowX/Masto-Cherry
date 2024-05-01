c6b2ce437d7456162fcc46e26caf9cac9dcd1a9c
Nathan Sparrow <24910097+DismalShadowX@users.noreply.github.com>
2024-05-01 01:11:02 -0400

    Update links.rb
    Signed-off-by: Nathan Sparrow <24910097+DismalShadowX@users.noreply.github.com>

--- app/models/trends/links.rb
+++ app/models/trends/links.rb
@@ -6,10 +6,10 @@ class Trends::Links < Trends::Base
   BATCH_SIZE = 100
 
   self.default_options = {
-    threshold: 5,
+    threshold: 2,
     review_threshold: 3,
     max_score_cooldown: 2.days.freeze,
-    max_score_halflife: 8.hours.freeze,
+    max_score_halflife: 12.hours.freeze,
     decay_threshold: 1,
   }
 
