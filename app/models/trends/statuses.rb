d452d139d7b46c3fa8b02e7c381c4486f41ac404
Nathan Sparrow <24910097+DismalShadowX@users.noreply.github.com>
2024-07-29 14:31:08 -0400

    Update statuses.rb

--- app/models/trends/statuses.rb
+++ app/models/trends/statuses.rb
@@ -6,7 +6,7 @@ class Trends::Statuses < Trends::Base
   BATCH_SIZE = 100
 
   self.default_options = {
-    threshold: 2,
+    threshold: 1,
     review_threshold: 3,
     score_halflife: 12.hours.freeze,
     decay_threshold: 0.3,
